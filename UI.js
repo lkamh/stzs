28098daaa4a5747b9a1a40dd601cf458e3ec5282
xlong <xlong1060@gmail.com>
2024-01-07 19:56:53 +0800

    Update UI.js

--- UI.js
+++ UI.js
@@ -13,57 +13,84 @@ ui.layout(
         <vertical>
             <appbar>
                 <toolbar id="toolbar" bg="#009688" title="刷题助手" />
+                <tabs id="tabs" bg="#009688" />
             </appbar>
-            <frame >
-                <vertical>
-                    <horizontal gravity="center_vertical" padding="5 20" >
-                        <View bg="#3399ff" h="*" w="8"  ></View>
-                        <horizontal gravity="center">
-                            <Switch id="autoService" text="  无障碍服务" checked="{{auto.service != null}}" gravity="left" textSize="18sp" />
-                            <Switch id="consoleshow" text="    悬浮窗权限" checked="{{(new android.provider.Settings).canDrawOverlays(context) != false}}" gravity="right" textSize="18sp" />
+            <viewpager id="viewpager">
+                <frame >
+                    <vertical>
+                        <horizontal gravity="center_vertical" padding="5 20" >
+                            <View bg="#3399ff" h="*" w="8"  ></View>
+                            <horizontal gravity="center">
+                                <Switch id="autoService" text="  无障碍服务" checked="{{auto.service != null}}" gravity="left" textSize="17sp" />
+                                <Switch id="consoleshow" text="    悬浮窗权限" checked="{{floaty.checkPermission()}}" gravity="right" textSize="17sp" />
+                            </horizontal>
                         </horizontal>
-                    </horizontal>
-                    <vertical gravity="center">
-                        <card gravity="center" marginTop="10xp" w="100" h="30" cardCornerRadius="10" >
-                            <vertical gravity="center" bg="#03A6EF">
-                                <text text="脚本选择" gravity="center" />
-                            </vertical>
-                        </card>
-                    <card w="*" cardCornerRadius="10" gravity="center" marginTop="10">
-                        <vertical>
-                            <horizontal padding="16">
-                                <radiogroup id="radiogroup" checkedButton="@+id/radio1">
-                                    <radio id="radio1" text="挑战答题" />
-                                    <radio id="radio2" text="四人赛" />
-                                </radiogroup>
+                        <vertical gravity="center">
+                            <card gravity="center" marginTop="10xp" w="100" h="30" cardCornerRadius="10" >
+                                <vertical gravity="center" bg="#03A6EF">
+                                    <text text="脚本选择" textColor="#FFFFFF" gravity="center" />
+                                </vertical>
+                            </card>
+                            <card w="*" cardCornerRadius="10" gravity="center" marginTop="10">
+                                <vertical>
+                                    <horizontal padding="16">
+                                        <radiogroup id="radiogroup" checkedButton="@+id/radio1">
+                                            <radio id="radio1" text="挑战答题" />
+                                            <radio id="radio2" text="四人赛" />
+                                        </radiogroup>
+                                    </horizontal>
+                                </vertical>
+                            </card>
+                        </vertical>
+                        <vertical gravity="bottom" padding="5 20">
+                            <button id="start" text="开 始 学 习" textSize="20sp" color="#ffffff" bg="#3399ff" foreground="?selectableItemBackground" />
+                        </vertical>
+                    </vertical>
+                </frame>
+                <ScrollView>
+                    <frame>
+                        <vertical id="x x z s_pro" gravity="center">
+                            <horizontal gravity="center_vertical" padding="5 5" >
+                                <View bg="#3399ff" h="*" w="10"  ></View>
+                                <vertical padding="10 8" h="auto" w="0" layout_weight="1">
+                                    <text w="auto" textColor="#222222" textSize="15sp" text="每轮间隔延时" />
+                                </vertical>
+                                <input id="stzs_cycle_wait_time" marginLeft="4" marginRight="6" text="2" textSize="13sp" inputType="number" />
+                            </horizontal>
+                            <horizontal gravity="center_vertical" padding="5 5" >
+                                <View bg="#3399ff" h="*" w="10"  ></View>
+                                <vertical padding="10 8" h="auto" w="0" layout_weight="1">
+                                    <text w="auto" textColor="#222222" textSize="15sp" text="image控件无法识别重置次数" />
+                                </vertical>
+                                <input id="stzs_chongzhi_cishu" marginLeft="4" marginRight="6" text="2" textSize="13sp" inputType="number" />
+                            </horizontal>
+                            <horizontal>
+                                <button style="Widget.AppCompat.Button.Colored" id="stzs_save" text="保存配置" padding="12dp" w="*" />
                             </horizontal>
                         </vertical>
-                    </card>
-                    </vertical>
-                    <vertical gravity="bottom" padding="5 20">
-                        <button id="start" text="开 始 学 习" textSize="20sp" color="#ffffff" bg="#3399ff" foreground="?selectableItemBackground" />
-                    </vertical>
-                </vertical>
-            </frame>
-        </vertical>
-        <vertical layout_gravity="left" bg="#ffffff" w="280">
-            <img w="280" h="200" scaleType="fitXY" src="http://images.shejidaren.com/wp-content/uploads/2014/10/023746fki.jpg" />
-            <list id="menu">
-                <horizontal bg="?selectableItemBackground" w="*">
-                    <img w="50" h="50" padding="16" src="{{this.icon}}" tint="{{color}}" />
-                    <text textColor="black" textSize="15sp" text="{{this.title}}" layout_gravity="center" />
-                </horizontal>
-            </list>
+                    </frame>
+                </ScrollView>
+            </viewpager>
         </vertical>
     </drawer>
 );
 
+activity.setSupportActionBar(ui.toolbar);
+// 设置滑动页面的标题
+ui.viewpager.setTitles(["首页", "脚本配置"]);
+// 让滑动页面和标签栏联动
+ui.tabs.setupWithViewPager(ui.viewpager);
+
+//运行前配置
+var STZS_CONFIG = storages.create("STZS_CONFIG");
+Initialize();
 http.__okhttp__.setTimeout(10000);
 
+
 var idx_dict = {
-      "46": 0,
-      "49": 1
-    };
+    "48": 0,
+    "51": 1
+};
 // 下载并运行所选脚本
 ui.start.click(function () {
     // threads.shutDownAll();
@@ -73,7 +100,7 @@ ui.start.click(function () {
     // }
     console.log("选择ID" + ui.radiogroup.getCheckedRadioButtonId())
     threads.start(function () {
-        execution = engines.execScript("学习助手", getScript(idx_dict[ui.radiogroup.getCheckedRadioButtonId()]));//直接下载0.js
+        execution = engines.execScript("刷题助手", getScript(idx_dict[ui.radiogroup.getCheckedRadioButtonId()]));//直接下载0.js
         toastLog('脚本加载完成')
     });
 });
@@ -107,33 +134,22 @@ ui.emitter.on("options_item_selected", (e, item) => {
     }
     e.consumed = true;
 });
-activity.setSupportActionBar(ui.toolbar);
-
-// //两次才能返回
-// threads.start(function () {
-//     var isCanFinish = false;
-//     var isCanFinishTimeout;
-//     ui.emitter.on("back_pressed", e => {
-//         if (!isCanFinish) {
-//             isCanFinish = true;
-//             isCanFinishTimeout = setTimeout(() => {
-//                 toastLog("再返回一次就失去我了哟！");
-//                 isCanFinish = false;
-//             }, 500);
-//             e.consumed = true;
-//         } else {
-//             clearTimeout(isCanFinishTimeout);
-//             e.consumed = false;
-//         };
-//     });
-//     setInterval(() => { }, 1000)
-// });
+// 读取脚本设置
+function Initialize() {
+    ui.stzs_cycle_wait_time.setText(STZS_CONFIG.get("cycle_wait_time", "1100"));
+    ui.stzs_chongzhi_cishu.setText(STZS_CONFIG.get("chongzhi_cishu", "10"));
+}
+// 保存刷题助手脚本设置
+ui.stzs_save.click(function () {
+    STZS_CONFIG.put("cycle_wait_time", ui.stzs_cycle_wait_time.getText() + "");
+    STZS_CONFIG.put("chongzhi_cishu", ui.stzs_chongzhi_cishu.getText() + "");
+    toastLog("配置保存成功！");
+})
 
 function getScript(choice) {
     let url_prefix = [
-        'https://mirror.ghproxy.com/https://raw.githubusercontent.com/lkamh/stzs/main/',
-        'https://raw.fastgit.org/lkamh/stzs/main/',
-        'https://gh.api.99988866.xyz/https://raw.githubusercontent.com/lkamh/stzs/main/'
+        'https://gh.xlong1060.top/https://raw.githubusercontent.com/lkamh/stzs/main/',
+        'https://mirror.ghproxy.com/https://raw.githubusercontent.com/lkamh/stzs/main/'
     ];
     for (var i = 0; i < url_prefix.length; i++) {
         try {
@@ -152,63 +168,63 @@ function getScript(choice) {
     return UI;
 }
 
-function strToArr(str) {
-    if (!str) {
-        return [];
-    }
-    //防止保活时,连环回调放入空字符,去掉末尾没用的字符串
-    return str.replace(/:$/, "").split(":");
-}
-try {
-    importClass(android.os.Handler);
-    importClass(android.database.ContentObserver);
-    let curPackage = auto.service ? currentPackage() : "com.lkamh.stzs"
+// function strToArr(str) {
+//     if (!str) {
+//         return [];
+//     }
+//     //防止保活时,连环回调放入空字符,去掉末尾没用的字符串
+//     return str.replace(/:$/, "").split(":");
+// }
+// try {
+//     importClass(android.os.Handler);
+//     importClass(android.database.ContentObserver);
+//     let curPackage = auto.service ? currentPackage() : "com.lkamh.stzs"
 
-    //保活白名单数组,也可以时其他应用的服务名,这里是autojspro的
-    const whiteList = [curPackage + "/com.stardust.xxzs.AccessibilityService"];
-    const contentResolver = context.getContentResolver();
-    let lastArr = strToArr(Settings.Secure.getString(contentResolver, "enabled_accessibility_services"));
-    let contentObserver = JavaAdapter(
-        ContentObserver, {
-            onChange(b) {
-                let service = "";
-                let str = Settings.Secure.getString(contentResolver, "enabled_accessibility_services");
-                let newArr = strToArr(str);
-                if (newArr.length > lastArr.length) {
-                    newArr.some(item => {
-                        service = item;
-                        return !lastArr.includes(item);
-                    });
-                    console.log("开启了----", service);
-                } else if (newArr.length < lastArr.length) {
-                    lastArr.some(item => {
-                        service = item;
-                        return !newArr.includes(item);
-                    });
-                    //这里可以做一些保活处理
-                    if (service && whiteList.includes(service)) {
-                        try {
-                            newArr.push(service);
-                            let success = Settings.Secure.putString(contentResolver, "enabled_accessibility_services", newArr.join(":"));
-                            console.log(`${success ? "保活成功" : "保活失败"}----${service}`);
-                        } catch (error) {
-                            console.log("没有权限----", error);
-                        }
-                    } else {
-                        console.log("关闭了----", service);
-                    }
-                }
-                lastArr = newArr;
-            },
-        },
-        new Handler()
-    );
-    contentResolver.registerContentObserver(Settings.Secure.getUriFor("enabled_accessibility_services"), true, contentObserver);
-    events.on("exit", () => {
-        contentResolver.unregisterContentObserver(contentObserver);
-    });
-} catch (e) {
-    console.error(e);
-}
+//     //保活白名单数组,也可以时其他应用的服务名,这里是autojspro的
+//     const whiteList = [curPackage + "/com.stardust.xxzs.AccessibilityService"];
+//     const contentResolver = context.getContentResolver();
+//     let lastArr = strToArr(Settings.Secure.getString(contentResolver, "enabled_accessibility_services"));
+//     let contentObserver = JavaAdapter(
+//         ContentObserver, {
+//         onChange(b) {
+//             let service = "";
+//             let str = Settings.Secure.getString(contentResolver, "enabled_accessibility_services");
+//             let newArr = strToArr(str);
+//             if (newArr.length > lastArr.length) {
+//                 newArr.some(item => {
+//                     service = item;
+//                     return !lastArr.includes(item);
+//                 });
+//                 console.log("开启了----", service);
+//             } else if (newArr.length < lastArr.length) {
+//                 lastArr.some(item => {
+//                     service = item;
+//                     return !newArr.includes(item);
+//                 });
+//                 //这里可以做一些保活处理
+//                 if (service && whiteList.includes(service)) {
+//                     try {
+//                         newArr.push(service);
+//                         let success = Settings.Secure.putString(contentResolver, "enabled_accessibility_services", newArr.join(":"));
+//                         console.log(`${success ? "保活成功" : "保活失败"}----${service}`);
+//                     } catch (error) {
+//                         console.log("没有权限----", error);
+//                     }
+//                 } else {
+//                     console.log("关闭了----", service);
+//                 }
+//             }
+//             lastArr = newArr;
+//         },
+//     },
+//         new Handler()
+//     );
+//     contentResolver.registerContentObserver(Settings.Secure.getUriFor("enabled_accessibility_services"), true, contentObserver);
+//     events.on("exit", () => {
+//         contentResolver.unregisterContentObserver(contentObserver);
+//     });
+// } catch (e) {
+//     console.error(e);
+// }
 //保持脚本运行
-setInterval(() => {}, 1000);
+setInterval(() => { }, 1000);
